53a5a46e2974aad7a303a07c1b66565c1f37ad66
allenliao666 <liao4792856@gmail.com>
2024-10-11 16:52:59 +0800

    Update 328.c

--- 328.c
+++ 328.c
@@ -6,18 +6,17 @@ struct ListNode {
 struct ListNode* oddEvenList(struct ListNode* head) {
     // if number of node is 0, 1, 2 return head
     if(!head || !head->next || !head->next->next) return head;
-    struct ListNode *even = head->next,*evenHead = head->next, *odd = head, *oddTail;
+    struct ListNode *even = head->next,*evenHead = head->next, *odd = head;
     
     //traverse the list, divide into 2 list(odd and even)
     while(even && even->next){
         odd->next = even->next;
         odd = odd->next;
-        oddTail = odd;  
         even->next = odd->next;
         even = even->next;
     }
 
     // Two lists finished, link oddTail to evenHead
-    oddTail->next = evenHead;
+    odd->next = evenHead;
     return head;
 }
